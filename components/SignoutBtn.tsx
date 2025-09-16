625de133e7d81fe0a8c18419c0a8d45cebe8190c
nikhil sai ankilla <nikhilsaiankilla@gmail.com>
2025-09-16 11:33:58 +0530

    fixed-errors

--- components/SignoutBtn.tsx
+++ components/SignoutBtn.tsx
@@ -1,19 +1,24 @@
 "use client"
 
-import React from 'react'
+import React, { useState } from 'react'
 import { Button } from './ui/button'
 import { userSignOut } from '@/actions/action'
 import { useRouter } from 'next/navigation'
+import { Loader } from 'lucide-react'
 
 const SignoutBtn = () => {
     const router = useRouter();
+    const [loading, setLoading] = useState(false);
+
     const handleSignout = async () => {
+        setLoading(true)
         await userSignOut();
         router.push('/')
+        setLoading(false)
     }
     return (
-        <Button onClick={handleSignout}>
-            logout
+        <Button onClick={handleSignout} className='cursor-pointer'>
+            {loading ? <Loader size={8} className='animate-spin text-white'/> : "logout"}
         </Button>
     )
 }
